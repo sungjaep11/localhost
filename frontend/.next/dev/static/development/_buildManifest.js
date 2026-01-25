3750ca026eff4f2d14e46ed240d46dee0fcc6515
배서연 <alicebsy@naver.com>
2026-01-25 17:31:30 +0900

    frontend Main에 푸시

--- frontend/.next/dev/static/development/_buildManifest.js
+++ frontend/.next/dev/static/development/_buildManifest.js
@@ -1,9 +1,10 @@
 self.__BUILD_MANIFEST = {
-  "/_error": [
-    "static/chunks/pages/_error.js"
-  ],
   "__rewrites": {
-    "afterFiles": [],
+    "afterFiles": [
+      {
+        "source": "/api/:path*"
+      }
+    ],
     "beforeFiles": [],
     "fallback": []
   },
--- frontend/.next/dev/types/routes.d.ts
+++ frontend/.next/dev/types/routes.d.ts
@@ -5,13 +5,14 @@ type AppRoutes = "/" | "/admin" | "/auth/login" | "/auth/signup" | "/game/[roomI
 type PageRoutes = never
 type LayoutRoutes = "/"
 type RedirectRoutes = never
-type RewriteRoutes = never
+type RewriteRoutes = "/api/[[...path]]"
 type Routes = AppRoutes | PageRoutes | LayoutRoutes | RedirectRoutes | RewriteRoutes
 
 
 interface ParamMap {
   "/": {}
   "/admin": {}
+  "/api/[[...path]]": { "path"?: string[]; }
   "/auth/login": {}
   "/auth/signup": {}
   "/game/[roomId]": { "roomId": string; }
